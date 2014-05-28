47a9de3556be3cb07c95ee2918fb990c0d3c6199
vincedotnet <vincedotnet@hotmail.com>
2014-05-28 23:25:23 +0800

    Update oss-receiver.php

--- public/oss/oss-receiver.php
+++ public/oss/oss-receiver.php
@@ -5,8 +5,8 @@ require ('../../extlib/OSS/aliyun.php');
 use \Aliyun\OSS\OSSClient;
 
 $client = OSSClient::factory(array(
-            'AccessKeyId' => '8urD64ete7oe9tZn',
-            'AccessKeySecret' => '5RY0EtXgmfYyCsOguzUalXgYJlka9x',
+            'AccessKeyId' => 'q4JANNcG8ki8hQRZ',
+            'AccessKeySecret' => 'z0BkNBjRBUU0gsobVupXJY8yHGXkFm',
         ));
 
 function putResourceObject(OSSClient $client, $bucket, $key, $content, $size) {
@@ -24,7 +24,7 @@ $filesize = $_FILES["file"]["size"];
 
 $keyId = '8urD64ete7oe9tZn';
 $keySecret = '5RY0EtXgmfYyCsOguzUalXgYJlka9x';
-$bucket = 'z2junjun';
+$bucket = 'angelhere-cheesetv';
 $key = $_FILES["file"]["name"];
 
 putResourceObject($client, $bucket, $key, fopen($file, 'r'), $filesize);
