90f3aa83febc36a4b50f73f60d005a55b2c6ec5d
vince <vince@vince-VirtualBox.(none)>
2014-06-01 09:04:39 +0800

    1

---
+++ application/modules/angel/controllers/ShowController.php
@@ -0,0 +1,28 @@
+<?php
+
+class Angel_ShowController extends Angel_Controller_Action {
+
+    protected $login_not_required = array('detail');
+
+    public function init() {
+        parent::init();
+        $this->_helper->layout->setLayout('main');
+    }
+
+    public function detailAction() {
+        $id = $this->request->getParam('id');
+        if ($id) {
+            $programModel = $this->getModel('program');
+            $program = $programModel->getById($id);
+            $this->view->model = $program;
+            $this->view->title = $program->name;
+            if ($program->oss_video) {
+                $this->view->video_url = $this->bootstrap_options['oss_prefix'] . $program->oss_video->key;
+            }
+            if ($program->oss_audio) {
+                $this->view->audio_url = $this->bootstrap_options['oss_prefix'] . $program->oss_audio->key;
+            }
+        }
+    }
+
+}
