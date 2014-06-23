f3509ca8a37e85c093271c8313be9e75760d62f0
vince <vince@vince-VirtualBox.(none)>
2014-06-23 17:12:27 +0800

    1

--- application/modules/angel/models/User.php
+++ application/modules/angel/models/User.php
@@ -34,12 +34,16 @@ class Angel_Model_User extends Angel_Model_AbstractModel {
     }
 
     public function addManageUser($email, $password, $salt, $checkemail = true) {
-
         $usertype = "admin";
-        return $this->registerUser($email, $password, $usertype, $salt, $checkemail);
+        return $this->registerUser($email, $password, uniqid(), $usertype, $salt, $checkemail);
     }
 
-    protected function registerUser($email, $password, $usertype, $salt, $checkemail) {
+    public function addUser($email, $password, $username, $salt, $checkemail = true) {
+        $usertype = "user";
+        return $this->registerUser($email, $password, $username, $usertype, $salt, $checkemail);
+    }
+
+    protected function registerUser($email, $password, $username, $usertype, $salt, $checkmail) {
         $result = false;
         if (empty($email)) {
             throw new Angel_Exception_User(Angel_Exception_User::EMAIL_EMPTY);
@@ -57,6 +61,7 @@ class Angel_Model_User extends Angel_Model_AbstractModel {
         $user = new $this->_document_class();
 
         $user->email = $email;
+        $user->username = $username;
         $user->salt = $salt;
         $user->user_type = $usertype;
         $user->password = $password;
@@ -83,67 +88,6 @@ class Angel_Model_User extends Angel_Model_AbstractModel {
         if ($result && !$checkemail) {
             
         }
-        return $result;
-    }
-
-    /**
-     * 用户注册
-     * 
-     * @param string $user_type
-     * @param string $email
-     * @param string $username
-     * @param string $password
-     * @param string $salt
-     * @param boolean $checkemail   -   是否要发送email验证邮件
-     * @return mix - when user registration success, return the user id, otherwise, boolean false
-     * @throws Angel_Exception_User 
-     */
-    public function addUser($user_type, $email, $username, $password, $salt, $checkemail = true) {
-        $result = false;
-
-        if (empty($email)) {
-            throw new Angel_Exception_User(Angel_Exception_User::EMAIL_EMPTY);
-        } else {
-            $validation = new Zend_Validate_EmailAddress();
-            if (!$validation->isValid($email)) {
-                throw new Angel_Exception_User(Angel_Exception_User::EMAIL_INVALID);
-            } else {
-                if ($this->isEmailExist($email)) {
-                    throw new Angel_Exception_User(Angel_Exception_User::EMAIL_NOT_UNIQUE);
-                }
-            }
-        }
-
-        $user = new $this->_document_class();
-
-        if (!$user->isUsertypeValid($user_type)) {
-            throw new Angel_Exception_User(Angel_Exception_User::USERTYPE_INVALID);
-        }
-
-        $user->user_type = $user_type;
-        $user->email = $email;
-        $user->username = $username;
-        $user->salt = $salt;
-        $user->password = $password;
-        $user->active_bln = true;
-        $user->email_validated_bln = !$checkemail;
-        $user->validated_bln = false;
-
-        try {
-            $this->_dm->persist($user);
-            $this->_dm->flush();
-
-            $result = $user->id;
-        } catch (Exception $e) {
-            $this->_logger->info(__CLASS__, __FUNCTION__, $e->getMessage() . "\n" . $e->getTraceAsString());
-            throw new Angel_Exception_User(Angel_Exception_User::ADD_USER_FAIL);
-        }
-
-        // send email to the new user to notice him to active his account
-        if ($result && $checkemail) {
-            $this->sendAccountValidationEmail($user);
-        }
-
         return $result;
     }
 
