09f603f940822862a15f3af57b8e259e29f32d29
ud223 <108234154@qq.com>
2014-09-10 20:43:49 +0800

    1

--- application/modules/angel/controllers/ShowController.php
+++ application/modules/angel/controllers/ShowController.php
@@ -33,21 +33,6 @@ class Angel_ShowController extends Angel_Controller_Action {
         }
     }
 
-    private function isMobile() {
-        $mobile = array();
-        static $mobilebrowser_list = 'Mobile|iPhone|Android|WAP|NetFront|JAVA|OperasMini|UCWEB|WindowssCE|Symbian|Series|webOS|SonyEricsson|Sony|BlackBerry|Cellphone|dopod|Nokia|samsung|PalmSource|Xphone|Xda|Smartphone|PIEPlus|MEIZU|MIDP|CLDC';
-        //note 获取手机浏览器
-        if (preg_match("/$mobilebrowser_list/i", $_SERVER['HTTP_USER_AGENT'], $mobile)) {
-            return true;
-        } else {
-            if (preg_match('/(mozilla|chrome|safari|opera|m3gate|winwap|openwave)/i', $_SERVER['HTTP_USER_AGENT'])) {
-                return false;
-            } else {
-                if ($_GET['mobile'] === 'yes') {
-                    return true;
-                } else {
-                    return false;
-    
     public function phonePlayAction() {
         $recommendModel = $this->getModel('recommend');
         $specialModel = $this->getModel('special');
