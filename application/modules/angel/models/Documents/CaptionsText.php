17c8cefb42af3351a1836f9940c6540251ce05da
ud223 <108234154@qq.com>
2014-10-16 17:18:04 +0800

    1

---
+++ application/modules/angel/models/Documents/CaptionsText.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Documents;
+
+use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
+
+/** @ODM\Document */
+class CaptionsText  extends AbstractDocument{
+    /** @ODM\String */
+    protected $text;
+    
+    /** @ODM\String */
+    protected $program_id;
+    
+    /** @ODM\Float */
+    protected $time_at;
+    
+    /** @ODM\Int */
+    protected $up;
+    
+    /** @ODM\ReferenceOne(targetDocument="\Documents\User") */
+    protected $user;
+    
+    /** @ODM\String */
+    protected $type;
+    
+    /** @ODM\Int */
+    protected $hot;
+}
