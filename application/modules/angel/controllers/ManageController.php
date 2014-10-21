36d356808ee2765179aaeb33bb69db405a5b02d0
ud223 <108234154@qq.com>
2014-10-21 12:04:06 +0800

    1

--- application/modules/angel/controllers/ManageController.php
+++ application/modules/angel/controllers/ManageController.php
@@ -1944,7 +1944,7 @@ class Angel_ManageController extends Angel_Controller_Action {
                     $resource[$cur_date]++; 
                 }
                 else {
-                    $resource[$cur_date] = 1;
+                    array_push($resource, array($cur_date => 1));
                 }
                 
                 $count++;
