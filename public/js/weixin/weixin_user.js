c9c4f3ddcd8faf2a1e1db3cca2b0d28533aef9dc
ud223 <108234154@qq.com>
2015-06-05 23:02:43 +0800

    1

--- public/js/weixin/weixin_user.js
+++ public/js/weixin/weixin_user.js
@@ -13,16 +13,16 @@ function userLogin() {
 
 function userLoginToMeet(meet_id) {
     alert(3);
-    user_id = localStorage.getItem('user_id');
+    //user_id = localStorage.getItem('user_id');
     var url = "http://cbook.test.angelhere.cn/reg/"+meet_id;
     alert(url);
-    alert(user_id);
+    //alert(user_id);
     alert(4);
-    if (!user_id) {
+    //if (!user_id) {
         alert(5);
         location.href = "https://open.weixin.qq.com/connect/oauth2/authorize?appid="+ appid +"&redirect_uri="+ url +"&response_type=code&scope=snsapi_userinfo&state=1#wechat_redirect";
-    }
-    else {
-
-    }
+    //}
+    //else {
+    //
+    //}
 }
