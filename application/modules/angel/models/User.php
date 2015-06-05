a64a472eb8b716fb6d715b92701eaf71c771f391
ud223 <108234154@qq.com>
2015-06-05 10:22:01 +0800

    1

--- application/modules/angel/models/User.php
+++ application/modules/angel/models/User.php
@@ -54,7 +54,7 @@ class Angel_Model_User extends Angel_Model_AbstractModel {
     public function getUserByOpenIds($openids) {
         $query = $this->_dm->createQueryBuilder($this->_document_class)->field('openid')->in($openids)->sort("created_at", 1);
 
-        $result = $query->getQuery()->execute();
+        $result = $query->getQuery();
 
         return $result;
     }
