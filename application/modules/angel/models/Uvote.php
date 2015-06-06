6f71d5aadfdbc83029b2d3460ee3bf4b51b5bc50
ud223 <108234154@qq.com>
2015-06-06 17:40:45 +0800

    1

---
+++ application/modules/angel/models/Uvote.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2015/6/6 0006
+ * Time: 下午 5:15
+ */
+
+class Angel_Model_Uvote extends Angel_Model_AbstractModel {
+    protected $_document_class = '\Documents\Uvote';
+
+    //记录用户活动日期投票
+    public function addUserVote($meet_id, $first_date, $second_date, $user_id) {
+        $data = array('meet_id'=> $meet_id,
+            'first_date'=> $first_date,
+            'second_date'=> $second_date,
+            'user_id'=> $user_id);
+
+        $result = $this->add($data);
+
+        return $result;
+    }
+} 
