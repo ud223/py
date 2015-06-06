543d826aaa72491d5f45c3663cf1b024302f904f
ud223 <108234154@qq.com>
2015-06-06 14:19:15 +0800

    1

--- application/modules/angel/controllers/ApiController.php
+++ application/modules/angel/controllers/ApiController.php
@@ -209,7 +209,7 @@ class Angel_ApiController extends Angel_Controller_Action {
             $start_date = $result->options_date[0];
             $end_date = $result->options_date[$length - 1];
 
-            $data = array("meet_text"=>$result->meet_text, "start_date"=>$start_date, "start_date"=>$end_date, "selected_date"=>$result->selected_date, "year"=>$result->year, "month"=>$result->month, "day"=>$result->day,"address"=>$result->address, "remark"=>$result->remark, "users"=>$users);
+            $data = array("meet_text"=>$result->meet_text, "start_date"=>$start_date, "end_date"=>$end_date, "selected_date"=>$result->selected_date, "year"=>$result->year, "month"=>$result->month, "day"=>$result->day,"address"=>$result->address, "remark"=>$result->remark, "users"=>$users);
 
 
             $this->_helper->json(array('data' => $data, 'code' => $code));
