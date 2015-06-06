4bd4f2ad8cc0d7ceedaacd36f1cd01afaefad74b
ud223 <108234154@qq.com>
2015-06-06 17:24:46 +0800

    1

--- application/modules/angel/models/UVote.php
+++ application/modules/angel/models/UVote.php
@@ -10,10 +10,10 @@ class Angel_Model_UVote extends Angel_Model_AbstractModel {
     protected $_document_class = '\Documents\UVote';
 
     //记录用户活动日期投票
-    public function addUserVote($meet_id, $first_date, $end_date, $user_id) {
+    public function addUserVote($meet_id, $first_date, $second_date, $user_id) {
         $data = array('meet_id'=> $meet_id,
             'first_date'=> $first_date,
-            'end_date'=> $end_date,
+            'second_date'=> $second_date,
             'user_id'=> $user_id);
 
         $result = $this->add($data);
