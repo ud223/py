b745116603e174a3e934425a16134e6242acad5c
ud223 <108234154@qq.com>
2015-06-10 12:29:36 +0800

    1

--- public/js/api/user.js
+++ public/js/api/user.js
@@ -3,14 +3,14 @@ function loadUser(user_id) {
     var url = '/api/meet/get';
 
     var  data = { 'user_id': user_id }
-    alert(user_id);
+    //alert(user_id);
     $.ajax({
         url: url,
         dataType: 'json',
         data: data,
         method: 'get',
         success: function (response) {
-            alert(JSON.stringify(response));
+            //alert(JSON.stringify(response));
             if (response.code == 200) {
                 //加载活动集合
                 loadUserInfo(response.data);
