f5f8d4de48ae110de0cb6a71c2984b529df07c98
ud223 <108234154@qq.com>
2015-06-11 10:10:11 +0800

    1

--- application/modules/angel/controllers/IndexController.php
+++ application/modules/angel/controllers/IndexController.php
@@ -179,7 +179,11 @@ class Angel_IndexController extends Angel_Controller_Action {
 
         $tmp_web_url = $this->getParam('web_url');
         $web_url = urldecode($tmp_web_url);
-//
+        exit($web_url);
+        if ($web_url == "http://cbook.test.angelhere.cn/") {
+            $web_url = "/";
+        }
+
         $web_url = str_replace("http://cbook.test.angelhere.cn/", "/", $web_url);
         exit($web_url);
         $open_id = $this->getOpenId($code);
