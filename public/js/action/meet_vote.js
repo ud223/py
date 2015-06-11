4432e445b90901459dd1e1cbdeeff5af3fa27a69
ud223 <108234154@qq.com>
2015-06-11 13:55:30 +0800

    1

--- public/js/action/meet_vote.js
+++ public/js/action/meet_vote.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
+    if (!user_id) {
+        localStorage.clear();
+    }
+    alert(user_id);
     //判断是否从后台得到用户的id，如果得到就不在从缓存中取用户id
     if (!user_id) {
         user_id = localStorage.getItem('user_id');
