b61422c23ca7def381f576b8faa9f2a3c50c8ee6
ud223 <108234154@qq.com>
2015-06-13 00:10:49 +0800

    1

--- public/js/action/meet_view.js
+++ public/js/action/meet_view.js
@@ -12,8 +12,8 @@ $(document).ready(function() {
     var user_id = localStorage.getItem('user_id');
 
     validUser(user_id)
-    //
-    //loadThisMeet(user_id);
+    
+    loadThisMeet(user_id);
     //loadProposerInfo(user_id);
     //
     //initBtnWord(user_id);
