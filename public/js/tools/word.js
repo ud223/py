fef2df563d1e0707f298c0f74779ee76b7e505cd
ud223 <108234154@qq.com>
2015-06-15 16:57:18 +0800

    1

--- public/js/tools/word.js
+++ public/js/tools/word.js
@@ -2,8 +2,8 @@ function loadWords(data) {
     $('#word-list').html('');
 
     if (data.length == 0) {
-        alert($('#test-tt').html());
-        $('#test-tt').html('暂无评论');
+        alert($('#test-tt'));
+        $('#test-tt').html('暂无留言');
 
         return;
     }
