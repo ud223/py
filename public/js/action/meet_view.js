64dfe0d0824c5ff47e4fde43c61ff9992b8b3492
ud223 <108234154@qq.com>
2015-06-16 12:46:54 +0800

    1

--- public/js/action/meet_view.js
+++ public/js/action/meet_view.js
@@ -72,6 +72,7 @@ function initBtnWord(user_id) {
 }
 //初始化后退按钮
 function initBtnBack(user_id) {
+    alert(1);
     $('#pge-cover-back').html("返回"+ nickname +"的云步客");
 
     $('#pge-cover-back').tap(function() {
