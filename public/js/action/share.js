aacd18c35fba15c00b55a171c51f83570db8d271
ud223 <108234154@qq.com>
2015-06-16 20:19:12 +0800

    1

--- public/js/action/share.js
+++ public/js/action/share.js
@@ -12,20 +12,16 @@ $(document).ready(function() {
     initBtnFriend();
     initBtnAdd();
     initBtnBack();
-    alert(5);
     initBtnShareMeet(user_id);
-    alert(6);
 })
 
 function initBtnShareMeet(user_id) {
-    alert(0);
     $('.glyphicon-share').tap(function () {
         shareMeet(user_id);
     });
 }
 
 function shareMeet(user_id){
-    alert(1);
     $('#sharing-bds').show();
     $('#sharing-bds').tap(function(){
         $('#sharing-bds').hide();
