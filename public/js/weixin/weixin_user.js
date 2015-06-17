8d1e2a9dd5c9c92b7b41c270d2547ff8d18862d0
ud223 <108234154@qq.com>
2015-06-17 13:36:24 +0800

    1

--- public/js/weixin/weixin_user.js
+++ public/js/weixin/weixin_user.js
@@ -22,11 +22,10 @@ function wx_Login(toUrl, fun_test_A, fun_test_B) {
     //微信开发环境
     if (env == 1) {
         var strUrl = toUrl.split("?");
-        var tmp_toUrl = encodeURI(strUrl[0]);
-
         //if (tmp_toUrl == "http://cbook.test.angelhere.cn/") {
         //    tmp_toUrl = "http://cbook.test.angelhere.cn"
         //}
+        var tmp_toUrl = strUrl[0];
         tmp_toUrl = tmp_toUrl.replace("http://cbook.test.angelhere.cn/", "/");
 
         if (tmp_toUrl.indexOf("#") < 0) {
@@ -39,7 +38,8 @@ function wx_Login(toUrl, fun_test_A, fun_test_B) {
             }
         }
         alert(tmp_toUrl);
-        var url = "http://cbook.test.angelhere.cn/reg?web_url=" + tmp_toUrl;
+        var temp_toUrl = encodeURI(tmp_toUrl);
+        var url = "http://cbook.test.angelhere.cn/reg?web_url=" + temp_toUrl;
         alert(url);
         location.href = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=" + appid + "&redirect_uri=" + url + "&response_type=code&scope=snsapi_userinfo&state=1#wechat_redirect";
 
