3d5c8fd4f8b6c8196fb11d221c66547dcfdb63fd
ud223 <108234154@qq.com>
2015-06-17 15:35:24 +0800

    1

--- public/js/weixin/weixin_user.js
+++ public/js/weixin/weixin_user.js
@@ -17,31 +17,54 @@ function userLoginToMeet(meet_id) {
 }
 
 function wx_Login(toUrl, fun_test_A, fun_test_B) {
+    //var env = validEnvironment();
+    //alert(toUrl);
+    ////微信开发环境
+    //if (env == 1) {
+    //    var strUrl = toUrl.split("?");
+    //    //if (tmp_toUrl == "http://cbook.test.angelhere.cn/") {
+    //    //    tmp_toUrl = "http://cbook.test.angelhere.cn"
+    //    //}
+    //    var tmp_toUrl = strUrl[0];
+    //    //tmp_toUrl = tmp_toUrl.replace("http://cbook.test.angelhere.cn/", "/");
+    //
+    //    if (tmp_toUrl.indexOf("#") < 0) {
+    //        if (strUrl.length > 1) {
+    //            var params = strUrl[1].split("#");
+    //
+    //            if (params.length > 1) {
+    //                tmp_toUrl = tmp_toUrl + "#" + params[1];
+    //            }
+    //        }
+    //    }
+    //    alert(tmp_toUrl);
+    //    alert(1);
+    //    var temp_toUrl = encodeURI(tmp_toUrl);
+    //    var url = "http://cbook.test.angelhere.cn/reg?web_url=" + temp_toUrl;
+    //    alert(url);
+    //    location.href = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=" + appid + "&redirect_uri=" + url + "&response_type=code&scope=snsapi_userinfo&state=1#wechat_redirect";
+    //
+    //    return 1;
+    //}
+    //else {
+    //    if (env == 2) {
+    //        initTestUser_A(fun_test_A);
+    //    }
+    //    else {
+    //        initTestUser_B(fun_test_B);
+    //    }
+    //
+    //    return 0;
+    //}
+
     var env = validEnvironment();
-    alert(toUrl);
+
     //微信开发环境
     if (env == 1) {
-        var strUrl = toUrl.split("?");
-        //if (tmp_toUrl == "http://cbook.test.angelhere.cn/") {
-        //    tmp_toUrl = "http://cbook.test.angelhere.cn"
-        //}
-        var tmp_toUrl = strUrl[0];
-        //tmp_toUrl = tmp_toUrl.replace("http://cbook.test.angelhere.cn/", "/");
+        var tmp_toUrl = encodeURI(toUrl.split("?")[0]);
 
-        if (tmp_toUrl.indexOf("#") < 0) {
-            if (strUrl.length > 1) {
-                var params = strUrl[1].split("#");
+        var url = "http://cbook.test.angelhere.cn/reg?web_url=" + tmp_toUrl;
 
-                if (params.length > 1) {
-                    tmp_toUrl = tmp_toUrl + "#" + params[1];
-                }
-            }
-        }
-        alert(tmp_toUrl);
-        alert(1);
-        var temp_toUrl = encodeURI(tmp_toUrl);
-        var url = "http://cbook.test.angelhere.cn/reg?web_url=" + temp_toUrl;
-        alert(url);
         location.href = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=" + appid + "&redirect_uri=" + url + "&response_type=code&scope=snsapi_userinfo&state=1#wechat_redirect";
 
         return 1;
