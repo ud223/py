54524a6bf85c82709a7b243d2cae4093f0bfac70
ud223 <108234154@qq.com>
2015-06-19 16:24:41 +0800

    1

--- public/js/api/word.js
+++ public/js/api/word.js
@@ -45,12 +45,9 @@ function queryWord(meet_id) {
         data: data,
         method: 'get',
         success: function (response) {
-            alert(JSON.stringify(response));
             if (response.code == 200) {
-                alert(99);
                 //加载活动集合
                 loadMeetWords(response.data);
-                alert(66);
             }
             else {
                 $.alertbox({ msg:response.data });
@@ -60,62 +57,4 @@ function queryWord(meet_id) {
 
         }
     });
-}
-
-function loadMeetWords(data) {
-    alert(0);
-    $('#view-word-list').html('');
-    alert(1);
-    alert(data.length);
-    if (data.length == 0) {
-        alert(3);
-        $('#view-test-tt').html('');//.html('暂无留言');
-        $('#view_word-list').hide();
-
-        return;
-    }
-
-    for (i = 0; i < data.length; i++) {
-        var node = $('#word_model').clone(true);
-
-        var date = data[i].date.date;
-
-        date = date.replace('-', '年');
-        date = date.replace('-', '月');
-        date = date.replace(' ', '日');
-
-        node.find('.ctxx').html(data[i].text);
-        node.find('.uune').html(data[i].user.nickname);
-        node.find('.untme').html(date);
-        node.find('.uxer').attr('src', data[i].user.headimgurl)
-
-        $('#view_word-list').append(node);
-    }
-
-    $('#vote-word-list').html('');
-
-    if (data.length == 0) {
-        alert(4);
-        $('#vote-test-tt').html('');//.html('暂无留言');
-        $('#vote_word-list').hide();
-
-        return;
-    }
-
-    for (i = 0; i < data.length; i++) {
-        var node = $('#word_model').clone(true);
-
-        var date = data[i].date.date;
-
-        date = date.replace('-', '年');
-        date = date.replace('-', '月');
-        date = date.replace(' ', '日');
-
-        node.find('.ctxx').html(data[i].text);
-        node.find('.uune').html(data[i].user.nickname);
-        node.find('.untme').html(date);
-        node.find('.uxer').attr('src', data[i].user.headimgurl)
-
-        $('#vote_word-list').append(node);
-    }
 }
