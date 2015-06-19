de4d79348a62011bc9e9e1c616b8fae2032bbca5
ud223 <108234154@qq.com>
2015-06-19 18:42:11 +0800

    1

--- public/js/weixin/weixin_user.js
+++ public/js/weixin/weixin_user.js
@@ -18,13 +18,11 @@ function userLoginToMeet(meet_id) {
 
 function wx_Login(toUrl, fun_test_A, fun_test_B) {
     var env = validEnvironment();
-    alert(toUrl);
     //微信开发环境
     if (env == 1) {
         var tmp_toUrl = encodeURI(toUrl.split("?")[0]);
-        alert(tmp_toUrl);
         var url = "http://cbook.test.angelhere.cn/reg?web_url=" + tmp_toUrl;
-        alert(url);
+
         location.href = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=" + appid + "&redirect_uri=" + url + "&response_type=code&scope=snsapi_userinfo&state=1#wechat_redirect";
 
         return 1;
