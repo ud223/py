6d93354dc7655961dd6bd1b15af7d0d276f77abe
ud223 <108234154@qq.com>
2015-06-20 16:27:25 +0800

    1

--- public/js/action/index.js
+++ public/js/action/index.js
@@ -82,7 +82,7 @@ function loadCalendar() {
 
         var user_id = localStorage.getItem('user_id');
 
-        alert(user_id);
+        //alert(user_id);
 
         meetModel.Query(user_id, event.year.valueOf(), event.month.valueOf(),  event.day.valueOf());
     });
