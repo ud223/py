89903af3f45efd58a9a0682c67cefeed73208b90
ud223 <108234154@qq.com>
2015-06-20 16:50:23 +0800

    1

--- public/js/action/common.js
+++ public/js/action/common.js
@@ -49,7 +49,7 @@ function closeMeetView() {
     var user_id = localStorage.getItem('user_id');
 
     $('.glyphicon-remove').tap(function() {
-        document.title = nickname + "日程安排";
+        document.title = nickname + "的云步客";
         $('#show-meet-view').hide();
         $('#show-meet-vote').hide();
         cur_meet_id = false;
