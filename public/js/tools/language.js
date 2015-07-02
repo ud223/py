57a1a869ba5d5807f7935703b8afdcad68fe2613
ud223 <108234154@qq.com>
2015-07-02 17:16:37 +0800

    1

--- public/js/tools/language.js
+++ public/js/tools/language.js
@@ -4,6 +4,27 @@ function setLanguage(language) {
     location.reload();
 }
 
+
+// JavaScript Document
+function loadjscssfile(filename,filetype){
+
+    if(filetype == "js"){
+        var fileref = document.createElement('script');
+        fileref.setAttribute("type","text/javascript");
+        fileref.setAttribute("src",filename);
+    }else if(filetype == "css"){
+
+        var fileref = document.createElement('link');
+        fileref.setAttribute("rel","stylesheet");
+        fileref.setAttribute("type","text/css");
+        fileref.setAttribute("href",filename);
+    }
+    if(typeof fileref != "undefined"){
+        document.getElementsByTagName("head")[0].appendChild(fileref);
+    }
+
+}
+
 $(document).ready(function() {
     var language = localStorage.getItem("language");
 
@@ -11,6 +32,8 @@ $(document).ready(function() {
         $(".cn").hide();
         $(".en").show();
         $(".en-select").addClass("selected");
+
+        loadjscssfile("/css/py_global_en.css","css");
     }
     else {
         $(".en").hide();
