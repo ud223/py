700d83b0cbeec1f34170759d1023d90f9162636c
ud223 <108234154@qq.com>
2015-07-03 11:44:48 +0800

    1

--- public/js/tools/language.js
+++ public/js/tools/language.js
@@ -1,19 +1,30 @@
 function setLanguage(language) {
     localStorage.setItem("language", language);
 
-    location.reload();
+    url = location.href;
+
+    if (language == "en") {
+        url = url.replace("?language=cn", "?language=en");
+
+        if (url.indexOf("?language=en") < 0) {
+            url = url + "?language=en";
+        }
+    }
+    else {
+        url = url.replace("?language=en", "");
+    }
+
+    location.href = url;
 }
 
 
 // JavaScript Document
 function loadjscssfile(filename,filetype){
-
     if(filetype == "js"){
         var fileref = document.createElement('script');
         fileref.setAttribute("type","text/javascript");
         fileref.setAttribute("src",filename);
     }else if(filetype == "css"){
-
         var fileref = document.createElement('link');
         fileref.setAttribute("rel","stylesheet");
         fileref.setAttribute("type","text/css");
@@ -22,11 +33,21 @@ function loadjscssfile(filename,filetype){
     if(typeof fileref != "undefined"){
         document.getElementsByTagName("head")[0].appendChild(fileref);
     }
-
 }
 
 $(document).ready(function() {
     var language = localStorage.getItem("language");
+
+    var url = location.href;
+
+    if (url.indexOf("?language=en") > -1) {
+        language = "en";
+    }
+    else {
+        language = "cn";
+    }
+
+    localStorage.setItem("language", language);
 
     if (language == "en") {
         $(".cn").hide();
