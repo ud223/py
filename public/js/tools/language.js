32986501c16b1b5527b9dd85a8d27ff2d45157ae
ud223 <108234154@qq.com>
2015-12-10 11:47:02 +0800

    1

--- public/js/tools/language.js
+++ public/js/tools/language.js
@@ -55,12 +55,12 @@ $(document).ready(function() {
         $(".en-select").addClass("selected");
 
         loadjscssfile("/css/py_global_en.css","css");
-        document.title = 'Shenzhen Puyuan New Materials Co. Ltd';
+        document.title = 'Color plate - Sandwich panels - Color steel sandwich - Steel tile - A fireproof board - Shenzhen Puyuan New Materials Co. Ltd';
     }
     else {
         $(".en").hide();
         $(".cn").show();
         $(".cn-select").addClass("selected");
-        document.title = '深圳市普源新型材料有限公司';
+        document.title = '彩钢板-彩钢夹心板-夹心彩钢板-彩钢瓦-防火A级板房-深圳市普源新型材料有限公司';
     }
 });
